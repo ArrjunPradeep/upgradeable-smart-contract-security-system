d1b7588917d2f08aa51a63dfb5aee424803334f9
ArrjunPradeep <arjunpradeepm17@gmail.com>
2024-08-09 00:17:15 +0530

    common-module-added

---
+++ security-microservices/libs/common/src/common.module.ts
@@ -0,0 +1,19 @@
+import { Global, Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import config from './config';
+
+@Global()
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['env/app.env', 'env/database.env', 'env/blockchain.env', 'env/mail.env'], // Add paths to your environment files
+      load: config,
+      cache: true,
+      expandVariables: true,
+      isGlobal: true,
+    }),
+  ],
+  exports: [ConfigModule],
+})
+
+export class CommonModule {}
