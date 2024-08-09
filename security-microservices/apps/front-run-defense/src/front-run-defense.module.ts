1b350b5be6331abe1273680212979058ec5de4ec
ArrjunPradeep <arjunpradeepm17@gmail.com>
2024-08-09 21:05:32 +0530

    front-run-defense-module-updated

--- security-microservices/apps/front-run-defense/src/front-run-defense.module.ts
+++ security-microservices/apps/front-run-defense/src/front-run-defense.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { FrontRunDefenseController } from './front-run-defense.controller';
 import { FrontRunDefenseService } from './front-run-defense.service';
 import { CommonModule } from '@app/common/common.module';
 
 @Module({
   imports: [CommonModule],
-  controllers: [FrontRunDefenseController],
+  controllers: [],
   providers: [FrontRunDefenseService],
 })
 export class FrontRunDefenseModule {}
