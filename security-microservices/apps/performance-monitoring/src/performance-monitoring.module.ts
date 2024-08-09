889ccbe5d5f035a57631731a38153922d24d1765
ArrjunPradeep <arjunpradeepm17@gmail.com>
2024-08-09 22:43:18 +0530

    performance-module-updated

--- security-microservices/apps/performance-monitoring/src/performance-monitoring.module.ts
+++ security-microservices/apps/performance-monitoring/src/performance-monitoring.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { PerformanceMonitoringController } from './performance-monitoring.controller';
 import { PerformanceMonitoringService } from './performance-monitoring.service';
 import { CommonModule } from '@app/common/common.module';
 
 @Module({
   imports: [CommonModule],
-  controllers: [PerformanceMonitoringController],
+  controllers: [],
   providers: [PerformanceMonitoringService],
 })
 export class PerformanceMonitoringModule {}
