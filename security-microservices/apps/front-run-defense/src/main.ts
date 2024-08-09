91ffb6487af118404673f479fbdb35c55111da5d
ArrjunPradeep <arjunpradeepm17@gmail.com>
2024-08-09 21:04:56 +0530

    front-run-defense-main-updated

--- security-microservices/apps/front-run-defense/src/main.ts
+++ security-microservices/apps/front-run-defense/src/main.ts
@@ -1,19 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { FrontRunDefenseModule } from './front-run-defense.module';
+import { FrontRunDefenseService } from './front-run-defense.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(FrontRunDefenseModule);
 
   const configService = app.get(ConfigService);
 
+  // Fetch configuration for the HTTP server
   const host = configService.get<string>('FRONT_RUN_DEFENSE.HOST');
   const port = configService.get<number>('FRONT_RUN_DEFENSE.PORT');
 
-  await app.listen(port, host, () => {
-    console.log(`FRONT_RUN_DEFENSE is running on http://${host}:${port}`);
-  });
+  // Get the FrontRunDefenseService instance
+  const frontRunDefenseService = app.get(FrontRunDefenseService);
+
+  // Start monitoring for suspicous transactions and frontrun
+  await frontRunDefenseService.monitorMempool();
+
+  // Start the HTTP server (if needed)
+  // await app.listen(port, host, () => {
+  //   console.log(`FRONT_RUN_DEFENSE is running on http://${host}:${port}`);
+  // });
 }
 
 bootstrap();
-
