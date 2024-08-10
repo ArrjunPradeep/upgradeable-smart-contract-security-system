0b0dc6d9c51964c36b49ec6b9789d048826ef68e
ArrjunPradeep <arjunpradeepm17@gmail.com>
2024-08-10 21:43:43 +0530

    postgres-database-config-added

--- security-microservices/libs/common/src/common.module.ts
+++ security-microservices/libs/common/src/common.module.ts
@@ -1,6 +1,7 @@
 import { Global, Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import config from './config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Global()
 @Module({
@@ -11,6 +12,20 @@ import config from './config';
       cache: true,
       expandVariables: true,
       isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async(configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.getOrThrow<string>('DATABASE.HOST'),
+        port: configService.getOrThrow<number>('DATABASE.PORT'),
+        username: configService.getOrThrow<string>('DATABASE.USERNAME'),
+        password: configService.getOrThrow<string>('DATABASE.PASSWORD'),
+        database: configService.getOrThrow<string>('DATABASE.NAME'),
+        autoLoadEntities: configService.getOrThrow<boolean>('DATABASE.AUTO_LOAD_ENTITIES'),
+        synchronize: configService.getOrThrow<boolean>('DATABASE.SYNCHRONIZE')
+      }),
+      inject: [ConfigService]
     }),
   ],
   exports: [ConfigModule],
