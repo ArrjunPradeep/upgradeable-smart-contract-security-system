ceefa1f4406321d99241c41e4b2fc89d713cc9e5
ArrjunPradeep <arjunpradeepm17@gmail.com>
2024-08-10 14:59:06 +0530

    deploy-scripts-updated

--- vulnerable-upgradeable-contracts/script/DeployVulnerable.s.sol
+++ vulnerable-upgradeable-contracts/script/DeployVulnerable.s.sol
@@ -5,6 +5,8 @@ pragma solidity ^0.8.20;
 import {Script} from "forge-std/Script.sol";
 import {VulnerableUpgradeableContract} from "../src/VulnerableUpgradeableContract.sol";
 import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import {Upgrades} from "openzeppelin-foundry-upgrades/Upgrades.sol";
+
 
 contract DeployVulnerable is Script {
     function run() external returns (address) {
@@ -13,11 +15,9 @@ contract DeployVulnerable is Script {
     }
 
     function deployVulnerable() public returns (address) {
-        vm.startBroadcast();
+        vm.startBroadcast();        
         VulnerableUpgradeableContract vulnerableContract = new VulnerableUpgradeableContract();
-        bytes memory data = abi.encodeWithSignature("initialize(address)", msg.sender);
-        ERC1967Proxy proxy = new ERC1967Proxy(address(vulnerableContract), data);
-        // VulnerableUpgradeableContract(address(proxy)).initialize(msg.sender);
+        ERC1967Proxy proxy = new ERC1967Proxy(address(vulnerableContract), "");
         vm.stopBroadcast();
         return address(proxy);
     }
